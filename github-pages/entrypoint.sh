fdefee4d9b98e626fd29c4d6f63183dac62e527e
Antoni Baum <antoni.baum@protonmail.com>
2020-06-12 19:07:03 +0200

    Fix

--- github-pages/entrypoint.sh
+++ github-pages/entrypoint.sh
@@ -1,10 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 cd $GITHUB_WORKSPACE/gh-pages
 
 cp -rf $GITHUB_WORKSPACE/gfx/ .
 cp -rf $GITHUB_WORKSPACE/interface .
 touch .nojekyll
+ls .github-pages
 mv -vn .github-pages/gitignore ./.gitignore
-echo "$INPUT_GOALS"
-python .github-pages/hoi4_icon_search_gen.py --goals "$INPUT_GOALS" --ideas "$INPUT_IDEAS" --texticons "$INPUT_TEXTICONS" --events "$INPUT_EVENTS" --news-events "$INPUT_NEWSEVENTS" --agencies "$INPUT_AGENCIES"  --decisions "$INPUT_DECISIONS" --decisions-cat "$INPUT_DECISIONSCAT" --decisions-pics "$INPUT_DECISIONPICS" --title "$INPUT_TITLE" --favicon "$INPUT_FAVICON" --modified-images-str "$INPUT_MODIFIEDFILES"
+read -a INPUT_GOALS <<< "${INPUT_GOALS}"
+read -a INPUT_IDEAS <<< "${INPUT_IDEAS}"
+read -a INPUT_TEXTICONS <<< "${INPUT_TEXTICONS}"
+read -a INPUT_EVENTS <<< "${INPUT_EVENTS}"
+read -a INPUT_NEWSEVENTS <<< "${INPUT_NEWSEVENTS}"
+read -a INPUT_AGENCIES <<< "${INPUT_AGENCIES}"
+read -a INPUT_DECISIONS <<< "${INPUT_DECISIONS}"
+read -a INPUT_DECISIONSCAT <<< "${INPUT_DECISIONSCAT}"
+read -a INPUT_DECISIONPICS <<< "${INPUT_DECISIONPICS}"
+python .github-pages/hoi4_icon_search_gen.py --goals ${INPUT_GOALS[@]} --ideas ${INPUT_IDEAS[@]} --texticons ${INPUT_TEXTICONS[@]} --events ${INPUT_EVENTS[@]} --news-events ${INPUT_NEWSEVENTS[@]} --agencies ${INPUT_AGENCIES[@]}  --decisions ${INPUT_DECISIONS[@]} --decisions-cat ${INPUT_DECISIONSCAT[@]} --decisions-pics ${INPUT_DECISIONPICS[@]} --title ${INPUT_TITLE} --favicon ${INPUT_FAVICON} --modified-images-str ${INPUT_MODIFIEDFILES}
