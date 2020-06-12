9bafbb21955425492c5724ca943ee274a0141156
Antoni Baum <antoni.baum@protonmail.com>
2020-06-12 18:39:47 +0200

    Debug

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -6,4 +6,5 @@ cp -rf $GITHUB_WORKSPACE/gfx/ .
 cp -rf $GITHUB_WORKSPACE/interface .
 touch .nojekyll
 mv -vn .github-pages/gitignore ./.gitignore
+echo "$INPUT_GOALS"
 python .github-pages/hoi4_icon_search_gen.py --goals "$INPUT_GOALS" --ideas "$INPUT_IDEAS" --texticons "$INPUT_TEXTICONS" --events "$INPUT_EVENTS" --news-events "$INPUT_NEWSEVENTS" --agencies "$INPUT_AGENCIES"  --decisions "$INPUT_DECISIONS" --decisions-cat "$INPUT_DECISIONSCAT" --decisions-pics "$INPUT_DECISIONPICS" --title "$INPUT_TITLE" --favicon "$INPUT_FAVICON" --modified-images-str "$INPUT_MODIFIEDFILES"
