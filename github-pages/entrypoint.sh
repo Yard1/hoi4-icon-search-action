37345d9202a1a89cd36e5bb4533cc974a5172f97
Antoni Baum <antoni.baum@protonmail.com>
2020-06-12 19:48:54 +0200

    Fix

--- github-pages/entrypoint.sh
+++ github-pages/entrypoint.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-cd /
-
-cp -rn hoi4-icon-search-action/* $GITHUB_WORKSPACE/gh-pages
-
-cd $GITHUB_WORKSPACE/gh-pages
+cd /hoi4-icon-search-action
 
 cp -rf $GITHUB_WORKSPACE/gfx/ .
 cp -rf $GITHUB_WORKSPACE/interface .
-touch .nojekyll
-cp -f github-pages/gitignore ./.gitignore
 read -a INPUT_GOALS <<< "${INPUT_GOALS}"
 read -a INPUT_IDEAS <<< "${INPUT_IDEAS}"
 read -a INPUT_TEXTICONS <<< "${INPUT_TEXTICONS}"
@@ -19,4 +13,7 @@ read -a INPUT_AGENCIES <<< "${INPUT_AGENCIES}"
 read -a INPUT_DECISIONS <<< "${INPUT_DECISIONS}"
 read -a INPUT_DECISIONSCAT <<< "${INPUT_DECISIONSCAT}"
 read -a INPUT_DECISIONPICS <<< "${INPUT_DECISIONPICS}"
-python github-pages/hoi4_icon_search_gen.py --goals ${INPUT_GOALS[@]} --ideas ${INPUT_IDEAS[@]} --texticons ${INPUT_TEXTICONS[@]} --events ${INPUT_EVENTS[@]} --news-events ${INPUT_NEWSEVENTS[@]} --agencies ${INPUT_AGENCIES[@]}  --decisions ${INPUT_DECISIONS[@]} --decisions-cat ${INPUT_DECISIONSCAT[@]} --decisions-pics ${INPUT_DECISIONPICS[@]} --title ${INPUT_TITLE} --favicon ${INPUT_FAVICON} --modified-images-str ${INPUT_MODIFIEDFILES}
+python github-pages/hoi4_icon_search_gen.py --goals ${INPUT_GOALS[@]} --ideas ${INPUT_IDEAS[@]} --texticons ${INPUT_TEXTICONS[@]} --events ${INPUT_EVENTS[@]} --news-events ${INPUT_NEWSEVENTS[@]} --agencies ${INPUT_AGENCIES[@]}  --decisions ${INPUT_DECISIONS[@]} --decisions-cat ${INPUT_DECISIONSCAT[@]} --decisions-pics ${INPUT_DECISIONPICS[@]} --title "${INPUT_TITLE}" --favicon "${INPUT_FAVICON}" --modified-images-str "${INPUT_MODIFIEDFILES}"
+touch $GITHUB_WORKSPACE/gh-pages/.nojekyll
+cp -f github-pages/gitignore $GITHUB_WORKSPACE/gh-pages/.gitignore
+mv index.html $GITHUB_WORKSPACE/gh-pages
