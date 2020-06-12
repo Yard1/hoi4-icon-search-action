0252e1a7db2ebcddeea6eeaf43bc05b97f258d17
Antoni Baum <antoni.baum@protonmail.com>
2020-06-12 23:49:07 +0200

    Fix path for decisionsPics

--- github-pages/entrypoint.sh
+++ github-pages/entrypoint.sh
@@ -12,8 +12,8 @@ read -a INPUT_NEWSEVENTS <<< "${INPUT_NEWSEVENTS}"
 read -a INPUT_AGENCIES <<< "${INPUT_AGENCIES}"
 read -a INPUT_DECISIONS <<< "${INPUT_DECISIONS}"
 read -a INPUT_DECISIONSCAT <<< "${INPUT_DECISIONSCAT}"
-read -a INPUT_DECISIONPICS <<< "${INPUT_DECISIONPICS}"
-python /hoi4-icon-search-action/github-pages/hoi4_icon_search_gen.py --goals ${INPUT_GOALS[@]//\"/} --ideas ${INPUT_IDEAS[@]//\"/} --texticons ${INPUT_TEXTICONS[@]//\"/} --events ${INPUT_EVENTS[@]//\"/} --news-events ${INPUT_NEWSEVENTS[@]//\"/} --agencies ${INPUT_AGENCIES[@]//\"/}  --decisions ${INPUT_DECISIONS[@]//\"/} --decisions-cat ${INPUT_DECISIONSCAT[@]//\"/} --decisions-pics ${INPUT_DECISIONPICS[@]//\"/} --title "${INPUT_TITLE}" --favicon "${INPUT_FAVICON}" --modified-images-str "${INPUT_MODIFIEDFILES}" --template-path "/hoi4-icon-search-action/github-pages/index.template"
+read -a INPUT_DECISIONSPICS <<< "${INPUT_DECISIONSPICS}"
+python /hoi4-icon-search-action/github-pages/hoi4_icon_search_gen.py --goals ${INPUT_GOALS[@]//\"/} --ideas ${INPUT_IDEAS[@]//\"/} --texticons ${INPUT_TEXTICONS[@]//\"/} --events ${INPUT_EVENTS[@]//\"/} --news-events ${INPUT_NEWSEVENTS[@]//\"/} --agencies ${INPUT_AGENCIES[@]//\"/}  --decisions ${INPUT_DECISIONS[@]//\"/} --decisions-cat ${INPUT_DECISIONSCAT[@]//\"/} --decisions-pics ${INPUT_DECISIONSPICS[@]//\"/} --title "${INPUT_TITLE}" --favicon "${INPUT_FAVICON}" --modified-images-str "${INPUT_MODIFIEDFILES}" --template-path "/hoi4-icon-search-action/github-pages/index.template"
 touch .nojekyll
 cp -f /hoi4-icon-search-action/github-pages/gitignore .gitignore
 cp -rf /hoi4-icon-search-action/images .
