369ef11d7ffdfb9ddcfbe2396cbecdb33001feff
Antoni Baum <antoni.baum@protonmail.com>
2020-06-12 19:55:50 +0200

    Debug

--- github-pages/entrypoint.sh
+++ github-pages/entrypoint.sh
@@ -13,6 +13,7 @@ read -a INPUT_AGENCIES <<< "${INPUT_AGENCIES}"
 read -a INPUT_DECISIONS <<< "${INPUT_DECISIONS}"
 read -a INPUT_DECISIONSCAT <<< "${INPUT_DECISIONSCAT}"
 read -a INPUT_DECISIONPICS <<< "${INPUT_DECISIONPICS}"
+ls
 python github-pages/hoi4_icon_search_gen.py --goals ${INPUT_GOALS[@]} --ideas ${INPUT_IDEAS[@]} --texticons ${INPUT_TEXTICONS[@]} --events ${INPUT_EVENTS[@]} --news-events ${INPUT_NEWSEVENTS[@]} --agencies ${INPUT_AGENCIES[@]}  --decisions ${INPUT_DECISIONS[@]} --decisions-cat ${INPUT_DECISIONSCAT[@]} --decisions-pics ${INPUT_DECISIONPICS[@]} --title "${INPUT_TITLE}" --favicon "${INPUT_FAVICON}" --modified-images-str "${INPUT_MODIFIEDFILES}"
 touch "$GITHUB_WORKSPACE/$INPUT_MODPATH/.nojekyll"
 cp -f github-pages/gitignore "$GITHUB_WORKSPACE/$INPUT_MODPATH/.gitignore"
